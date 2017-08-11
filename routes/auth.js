25b6e6381f559365b49e2e80e1ca58f76a0dbd89
Alfonso Antonio <j.alfonso.antonio@gmail.com>
2017-08-11 09:28:54 -0400

    initial commit

---
+++ routes/auth.js
@@ -0,0 +1,92 @@
+const router = require('express').Router();
+const passport = require('passport');
+
+router.get('/login', (req, res) => {
+
+  // const flashMessages = res.locals.getMessages();
+  // console.log('flash', flashMessages);
+
+  // if (flashMessages.error) {
+  //   res.render('login', {
+  //     showErrors: true,
+  //     errors: flashMessages.error
+  //   });
+  // }
+  // else {
+  //   res.render('login');
+  // }
+   res.render('login');
+});
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/home',
+  failureRedirect: '/login',
+  failureFlash: true
+}));
+
+router.get('/register', (req, res) => {
+
+  // const flashMessages = res.locals.getMessages();
+  // console.log('flash', flashMessages);
+  //
+  // if (flashMessages.error) {
+  //   res.render('register', {
+  //     showErrors: true,
+  //     errors: flashMessages.error
+  //   })
+  // }
+  // else {
+  //   res.render('register');
+  // }
+  res.render('register');
+});
+
+router.post('/register', (req, res, next) => {
+  console.log('body', req.body);
+
+  req.checkBody('username', 'Username is required.').notEmpty();
+  req.checkBody('password', 'Password is required.').notEmpty();
+
+  req.getValidationResult()
+    .then(function(result) {
+      if (result.isEmpty() === false) {
+        result.array().forEach((error) => {
+          req.flash('error', error.msg);
+        })
+        res.redirect('/register');
+      }
+      else {
+        const user = new User({ username: req.body.username, password: req.body.password });
+        user.save((err) => {
+          if (err) {
+
+            if (err.message.indexOf('duplicate key error') > -1) {
+              req.flash('error', 'Username already in use.');
+            }
+            else {
+              req.flash('error', 'There was a problem with your registration.');
+            }
+            res.redirect('/register');
+
+            console.log('There was an error saving the user.', err);
+          }
+          else {
+            next();
+          }
+        });
+      }
+    });
+
+
+
+
+}, passport.authenticate('local', {
+  successRedirect: '/home'
+}));
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/login');
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('express-flash-messages');
+const expressValidator = require('express-validator');
+
+
+const app = express();
+
+//static files
+app.use(express.static('public'));
+
+//session
+// app.use(session({
+//   secret: 'fadlkuanvdflbdissgndvdoerisfdslj',
+//   resave: false,
+//   saveUninitialized: false
+// }));
+
+//passport
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(flash());
+// require('./passportconfig').configure(passport);
+
+//handle post bodies
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(expressValidator());
+
+//mustache
+const mustache = mustacheExpress();
+mustache.cache = null;
+app.engine('mustache', mustache);
+app.set('view engine', 'mustache');
+
+app.use(require('./routes/general'));
+app.use(require('./routes/auth'));
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000.');
+});
